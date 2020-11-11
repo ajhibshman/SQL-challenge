b0ab58bba19770fc4c89efb7190687821c76c640
ajhibshman <ajhibshman@hotmail.com>
2020-11-11 11:53:18 -0700

    updating gitignore

--- queries.sql
+++ queries.sql
@@ -28,15 +28,32 @@ FROM employees AS e
 JOIN dept_emp AS d2 ON e.emp_no=d2.emp_no
 JOIN departments AS d ON d2.dept_no=d.dept_no;
 
---5)List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
 
+--list first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name,last_name,sex 
+FROM employees
+WHERE first_name='Hercules' AND last_name LIKE 'B%';
 
 --6)List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT e.emp_no,e.last_name,e.first_name,d.dept_name
+FROM employees AS e
+JOIN dept_emp AS d2 ON e.emp_no=d2.emp_no
+JOIN departments AS d ON d2.dept_no=d.dept_no
+WHERE d.dept_name = 'Sales';
 
 --7)List all employees in the Sales and Development departments, including their employee number, last name, first name, 
 -- and department name.
 
+SELECT e.emp_no,e.last_name,e.first_name,d.dept_name
+FROM employees AS e
+JOIN dept_emp AS d2 ON e.emp_no=d2.emp_no
+JOIN departments AS d ON d2.dept_no=d.dept_no
+WHERE d.dept_name IN ('Sales','Development');
+					  
 --8) In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
 
-
+SELECT last_name, COUNT (last_name)
+FROM employees
+GROUP BY last_name
+ORDER BY count DESC;
 --end
